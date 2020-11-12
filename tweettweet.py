d5fba4df0a318a730ec7b48a7b4716997c9ea841
abc <abc@example.com>
2020-11-12 10:31:31 +0200

    first commit

---
+++ tweettweet.py
@@ -0,0 +1,36 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+user = api.me()
+
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(300)
+
+
+search_string = 'python'
+numberOfTweets = 2
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('i liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+# Generous Bot
+# for follower in limit_handler(tweepy.Cursor(api.followers).item()):
+#     if follower.name == 'miguel':
+#         follower.follow()
+#         break
